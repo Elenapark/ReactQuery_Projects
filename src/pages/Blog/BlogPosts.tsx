ab5f7b385424e0e10e1acfe6294c2a8b9146910b
elenapark <elenability@naver.com>
2022-08-12 20:49:09 +0900

    add : query key 추가하여 페이지네이션 구현

--- src/pages/Blog/BlogPosts.tsx
+++ src/pages/Blog/BlogPosts.tsx
@@ -4,19 +4,25 @@ import PostDetails from "./PostDetails";
 
 const MAX_PAGES = 10;
 
-async function fetchPosts() {
+async function fetchPosts(page) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
   );
   return response.json();
 }
 
 const BlogPosts = () => {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
-  const { data, isLoading, isError, error } = useQuery("posts", fetchPosts, {
-    staleTime: 2000,
-  });
+  const { data, isLoading, isError, error } = useQuery(
+    // 의존성배열처럼 사용
+    // currentPage state가 바뀌면 쿼리 키가 바뀌므로 useQuery에 새로운 쿼리를 알려서 데이터를 다시 가져옴
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+    }
+  );
   // isFetching : the async query function hasn't yet resolved
   // isLoading : no cached data, plus isFetching
   if (isLoading) return <div>Loading...</div>;
@@ -43,11 +49,21 @@ const BlogPosts = () => {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= MAX_PAGES}
+          onClick={() => {
+            setCurrentPage((pre) => pre + 1);
+          }}
+        >
           Next page
         </button>
       </div>
